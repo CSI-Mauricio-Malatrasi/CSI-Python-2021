24c911ecf883506338acb754c928f84689f94fcb
CSI-Mauricio-Malatrasi <24-050@sanignacio.pr>
2021-10-12 10:37:07 -0400

    Experiment Data
    done

---
+++ Projects/Projectile-Motion/ExperimentData.py
@@ -0,0 +1,9 @@
+class ExperimentData:
+    def __init__(self, Gunname:str, Guncartridge:str, Round:str, Roundvelocity:float, Building:str, Buildingheight:float, gravity:float):
+        self.Gunname = Gunname,
+        self.Guncartridge = Guncartridge
+        self.Round = Round
+        self.Roundvelocity = Roundvelocity
+        self.Building = Building
+        self.Buildingheight = Buildingheight
+        self.gravity = gravity
--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,23 +1,33 @@
 import math
-# Variables of the gun.
+from ExperimentData import ExperimentData
 
 # Gunname = ("HK 416A5")
 # Guncartridge = ("5.56x45mm NATO")
 # Round = ("5.56x45mm HP")
 # Roundvelocity = 947 #m/s
-
-# Chose the building and assigned the variables.
-
 # Building = ("El Choli")
 # Buildingheight = 48.5 #meters
 # gravity = 9.8 #m/s
 
-def experiment( Gunname:str, Guncartridge:str, Round:str, Roundvelocity:float, Building:str, Buildingheight:float, gravity:float):
+def experiment(experimentData:ExperimentData):
     # Im calculating time and DeltaX.
 
-    time = (math.sqrt(2 * Buildingheight/gravity))
-    DeltaX = (Roundvelocity * time)
+    time = (math.sqrt(2 * experimentData.Buildingheight/experimentData.gravity))
+    DeltaX = (experimentData.Roundvelocity * time)
 
-    print(f"I shot a projectile from {Building}, it's {Buildingheight} meters high. It goes at {Roundvelocity} and it takes about {time} seconds to reach the ground. The distance covered is {DeltaX}.")
+    print(f"I shot a projectile from {experimentData.Building}, it's {experimentData.Buildingheight} meters high. It goes at {experimentData.Roundvelocity} and it takes about {time} seconds to reach the ground. The distance covered is {DeltaX}.")
 
-experiment("HK 416A5", "5.56x45mm NATO", "5.56x45mm HP", 947, "El Choli", 48.5, 9.8)
+
+# experimentData = {
+#     "Gunname" : "HK 416A5",
+#     "Guncartridge" : "5.56x45mm NATO",
+#     "Round" : "5.56x45mm HP",
+#     "Roundvelocity" : 947,
+#     "Building" : "El Choli",
+#     "Buildingheight" : 48.5,
+#     "gravity" : 9.8
+# }
+
+myData = ExperimentData("HK 416A5", "5.56x45mm NATO", "5.56x45mm HP", 947, "El Choli", 48.5, 9.8)
+
+experiment(myData)
