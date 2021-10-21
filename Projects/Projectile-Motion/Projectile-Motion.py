83000cb11122ec44df733efd8a4c7aef4305b397
CSI-Mauricio-Malatrasi <24-050@sanignacio.pr>
2021-10-21 10:31:43 -0400

    projectile motion
    json part

--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,5 +1,8 @@
 import math
+import os
 from ExperimentData import ExperimentData
+from pathlib import Path
+import json
 
 # Gunname = ("HK 416A5")
 # Guncartridge = ("5.56x45mm NATO")
@@ -8,6 +11,7 @@ from ExperimentData import ExperimentData
 # Building = ("El Choli")
 # Buildingheight = 48.5 #meters
 # gravity = 9.8 #m/s
+
 
 def experiment(experimentData:ExperimentData):
     # Im calculating time and DeltaX.
@@ -28,6 +32,17 @@ def experiment(experimentData:ExperimentData):
 #     "gravity" : 9.8
 # }
 
-myData = ExperimentData("HK 416A5", "5.56x45mm NATO", "5.56x45mm HP", 947, "El Choli", 48.5, 9.8)
+myDataSet = [
+    ExperimentData("HK 416A5", "5.56x45mm NATO", "5.56x45mm HP", 947, "El Choli", 48.5, 9.8),
+    ExperimentData("NSV Utes", "12.7x108mm", "12.7x108mm B-32 gl", 818, "El Choli", 48.5, 9.8),
+    ExperimentData("M1A", "7.62x51 mm NATO", "7.62x51 mm FMJ", 840, "El Choli", 48.5, 9.8),
+    ExperimentData("DVL-10", "7.62x51 mm NATO", "7.62x51 mm M61", 849, "El Choli", 48.5, 9.8),
+    ExperimentData("FN GL40", "40x46 mm", "40x46 mm M381 (HE) grenade", 76, "El Choli", 48.5, 9.8)
+]
+experiment(myDataSet[2])
 
-experiment(myData)
+myOutputPath = Path(__file__).parents[0]
+myOutputFilePath = os.path.join(myOutputPath, 'ExperimentData.json')
+
+with open(myOutputFilePath, 'w') as outfile:
+    json.dump(myDataSet[2].__dict__, outfile)
