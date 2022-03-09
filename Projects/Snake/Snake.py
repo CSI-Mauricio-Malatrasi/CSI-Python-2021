d5f7e017ead51d1b7b16674b51cf1c94ae392f41
CSI-Mauricio-Malatrasi <24-050@sanignacio.pr>
2022-03-09 10:11:08 -0400

    First block

---
+++ Projects/Snake/Snake.py
@@ -0,0 +1,6 @@
+import pygame #imports pygame
+pygame.init()   #starts game
+dis=pygame.display.set_mode((400,300))  #Dimensions
+pygame.display.update() #updates the game
+pygame.quit()   #quits the game
+quit()  #quits everything
