e3633575449bb3ad4b2aa6139fd6c6a8bb5dcfff
CSI-Mauricio-Malatrasi <24-050@sanignacio.pr>
2022-03-15 09:53:25 -0400

    Game over when snake reaches the corners

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -1,4 +1,5 @@
 import pygame #imports pygame
+import time     #imports time
 pygame.init()   #starts game
 
 #Colors RGB, Red Green Blue
@@ -6,18 +7,29 @@ white = (255, 255, 255) #white color
 black=(0,0,0)  #blue color
 red=(255,0,0)   #red color
 
+dis_width = 800     #width
+dis_height  = 600   #height
 dis=pygame.display.set_mode((800,600))  #Dimensions
 pygame.display.set_caption('Snake game by Edureka') #Displays the name of the game
 
 game_over=False #Tells you the game isn't over
 
-x1 = 300
-y1 = 300
+x1 = dis_width/2
+y1 = dis_height/2
+
+snake_block=10      #Shows the snake
  
 x1_change = 0       #Stores the change in the snake
 y1_change = 0       #Stores the change in the snake
 
-clock = pygame.time.Clock()
+clock = pygame.time.Clock()     #Clock for the game
+snake_speed=30      #speed of the snake
+
+font_style = pygame.font.SysFont(None, 50)  #Font size for the game
+
+def message(msg,color):         #Defines message
+    mesg = font_style.render(msg, True, color) 
+    dis.blit(mesg, [dis_width/2, dis_height/2])
 
 while not game_over:    #Start loops
     for event in pygame.event.get(): 
@@ -37,14 +49,21 @@ while not game_over:    #Start loops
                 y1_change = 10
                 x1_change = 0
 
+    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:     #Makes it so if you do those things, it makes game_over true.
+        game_over = True
+
     x1 += x1_change
     y1 += y1_change
-    dis.fill(white)
-    pygame.draw.rect(dis, black, [x1, y1, 10, 10])  #Draws the colors imported
+    dis.fill(white)     #full white background
+    pygame.draw.rect(dis, black, [x1, y1, snake_block, snake_block])  #Draws the colors imported
 
     pygame.display.update() #Updates the game
 
-    clock.tick(30)  #Makes a clock
+    clock.tick(snake_speed)  #Makes a clock
+
+message("You lost",red)     #Displays "You lost" in red in the middle of the screen
+pygame.display.update()
+time.sleep(2)
 
 pygame.quit()   #quits the game
 quit()  #quits everything
